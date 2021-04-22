7c8ca65d34479c1b4d8d8426739b29c2570ee865
Dell <saxena.aryan2000@gmail.com>
2021-04-23 03:05:26 +0530

    images changed

--- shopify/src/Home.js
+++ shopify/src/Home.js
@@ -11,17 +11,19 @@ function Home() {
             alt="diwali "/>
 
           <div className="home__row" >
-              <Product title='the lean startup' price={399}/>
-              <Product/>
+              <Product title='The immortals of meluha ' price={399} rating ={5} image="https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_700/MTc0NDQ0NTg4NzI1OTcwNTY2/shiva-myth-redefined-in-the-shiva-trilogy-by-amish-tripathy.jpg"/>
+              <Product title='iPhone by Apple Inc. 'price={45000} rating={4} image="https://photos.dialcom.lk/big/items/ip12-_3369_jpg"/>
               
               </div> 
               <div className="home__row" >
-              <Product/>
-              <Product/>
-              <Product/>
+              <Product title='High quality watch for men. Best smartwatch for men.' price={24999} rating={4} image="https://www.bhphotovideo.com/images/images2500x2500/apple_mlck2ll_a_watch_smartwatch_38mm_space_1187206.jpg"/>
+              <Product title='Discover Degrees full range of
+
+deodorants & antiperspirants ' price={249} rating={5} image="https://n2.sdlcdn.com/imgs/b/s/6/SDL374637059_1-e5e35.jpg"/>
+              <Product title='Farmhouse Wood Dining Table ' price={4999} rating={4} image="https://www.woodcraft.ca/wp-content/uploads/2018/10/Spotlight-On-Reclaimed-Wood-Dining-Tables-FEATURE.jpg"/>
               </div> 
               <div className="home__row" >
-              <Product/>
+              <Product title=' Grey Wallpaper for walls' price={2999} rating={5} image="https://cdn.shopify.com/s/files/1/0285/1316/products/w0558_1s_Black-grey-textured-rectangular-pattern-tile-wallpaper-Modern-Groove_For-Interior-Walls-7.jpg?v=1602869599"/>
               </div> 
         </div>
         </div>
--- shopify/src/Product.js
+++ shopify/src/Product.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import "./Product.css";
 
-function Product({title,image,price,rating}) {
+function Product({id,title,image,price,rating}) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The lean startup</p>
+                <p>{title}</p>
                 <p className="product__price">
                 <small>Rs</small>
-                <strong>399</strong></p>
+                <strong>{price}</strong></p>
                 <div className="product__rating">
+                    {Array(rating).fill().map((_,i)=>(
                     <p>*</p>
+                    ))}
                    
                 </div>
             </div>
-            <img src="https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_700/MTc0NDQ0NTg4NzI1OTcwNTY2/shiva-myth-redefined-in-the-shiva-trilogy-by-amish-tripathy.jpg" alt=""/>
+            <img src={image}alt=""/>
             <button>Add to Basket</button>
         </div>
     );
