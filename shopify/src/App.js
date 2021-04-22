e4e22a9a1db11eda1fa94b1b4ba34feca8e9e16d
Dell <saxena.aryan2000@gmail.com>
2021-04-23 00:18:47 +0530

    Building Home page

--- shopify/src/App.js
+++ shopify/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import {BrowserRouter as Router , Switch ,Route} from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
@@ -9,31 +10,17 @@ function App() {
     <div className="app">
     <Switch>
   <Route path="/checkout">
+   <Header/>
     <h1>Checkout</h1>
+    
   </Route>
   <Router path="/login">
     <h1>Login Page</h1>
   </Router>
   <Router path="/">
     <Header/>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
-    <h1> HOME PAGE!!</h1>
+    <Home/>
+    
     
   </Router>
 
---
+++ shopify/src/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.css';
+
+function Home() {
+    return (
+
+        <div className="home">
+            <div className="home__container">
+            <img className="home__image" src="https://images.unsplash.com/photo-1555529669-26f9d103abdd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+            alt="diwali "/>
+
+          <div className="home__row" >
+              
+              </div> 
+              <div className="home__row" >
+              
+              </div> 
+              <div className="home__row" >
+              
+              </div> 
+        </div>
+        </div>
+    )
+}
+
+export default Home;
